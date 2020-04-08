f5581b4daee25a1ba07d6cd0be04d3bd03b6afce
Birbit1488 <39192720+Birbit1488@users.noreply.github.com>
2020-04-08 13:07:01 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,11 +16,13 @@ from inline_keyboard import *
 API_TOKEN = config.token
 
 Bot = telebot.TeleBot(token=API_TOKEN)
-current_section = 'start'
+previous_section = 'start'
 
 
 @bot.message_handler(commands=['start'])
 def welcome(message):
+    global previous_section
+    previous_section = 'start'
     bot.reply_to(message, 'Hello World!')  # Приветствие, введение для пользователя
     welcome_keyboard = Keyboard(message)
     welcome_keyboard.start_keyboard()
@@ -28,26 +30,44 @@ def welcome(message):
 
 @bot.message_handler(regexp='F.A.Q.')
 def faq(message):
+    global previous_section
+    previous_section = 'faq_button_keyboard'
     faq_keyboard = Keyboard(message)
     faq_keyboard.faq_button_keyboard()
 
 
 @bot.message_handler(regexp='В главное меню')
 def back_to_main_menu(message):
+    global previous_section
+    previous_section = 'start'
     main_menu_keyboard = Keyboard(message)
     main_menu_keyboard.start_keyboard()
 
 
 @bot.message_handler(regexp='Stat')
 def statistics(message):
+    global previous_section
+    previous_section = 'start_keyboard()'
     stat_keyboard = Keyboard(message)
     stat_keyboard.stat_keyboard()
-    
+
 
 @bot.message_handler(regexp='Назад')
 def step_back(message):
     step_back_keyboard = Keyboard(message)
-    eval('step_back_keyboard.'+'stat_keyboard()')
-    
+    eval('step_back_keyboard.'+previous_section)
+
+
+@bot.message_handler(regexp='По России')
+def russian_stats(message):
+    russian_stats_keyboard = Keyboard(message)
+    russian_stats_keyboard.russia_keyboard()
+
+
+@bot.message_handler(regexp='По Миру')
+def world_stats(message):
+    world_stats_keyboard = Keyboard(message)
+    world_stats_keyboard.russia_keyboard()
+
 
 bot.polling(none_stop=True, interval=0)
